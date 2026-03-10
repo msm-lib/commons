7bad0a69c1689d70305bb3b92694a36c72ba7ff0
danhnh2 <danhnh@msm.com.vn>
2026-03-10 09:10:28 +0700

    Add default integrationType

--- src/main/java/com/msm/core/hook/GenericObjectHookMetadata.java
+++ src/main/java/com/msm/core/hook/GenericObjectHookMetadata.java
@@ -1,14 +1,21 @@
 package com.msm.core.hook;
 
+import com.msm.core.commons.Utils;
+
 public abstract class GenericObjectHookMetadata implements ObjectHookMetadata {
 
     @Override
     public String type() {
-        return objectName() + ":" + phase();
+        return Utils.STR.format(Constant.OBJECT_HOOK_KEY, objectName(), phase());
     }
 
     @Override
     public String objectName() {
-        return "GenericHookObject";
+        return Constant.GENERIC_OBJECT_HOOK_NAME;
+    }
+
+    @Override
+    public String integrationType() {
+        return Utils.STR.uncapitalize(getClass().getSimpleName());
     }
 }
