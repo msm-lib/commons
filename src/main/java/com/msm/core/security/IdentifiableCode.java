0d68577a2a6e60e0b10ba471cc2ca79a0df94544
danhnh2 <danhnh@msm.com.vn>
2026-06-25 15:02:19 +0700

    Update new PermissionAction

--- src/main/java/com/msm/core/security/IdentifiableCode.java
+++ src/main/java/com/msm/core/security/IdentifiableCode.java
@@ -1,15 +1,28 @@
 package com.msm.core.security;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public interface IdentifiableCode {
     String getCode();
 
-    static <E extends Enum<E> & IdentifiableCode> E fromCode(Class<E> enumType, String code) {
-        for (E value : enumType.getEnumConstants()) {
-            if (value.getCode().equals(code)) {
-                return value;
-            }
-        }
+    Map<Class<?>, Map<String, ?>> CACHE = new ConcurrentHashMap<>();
 
-        throw new IllegalArgumentException("Unknown code: " + code);
+    @SuppressWarnings("unchecked")
+    static <E extends Enum<E> & IdentifiableCode> E fromCode(
+            Class<E> enumType,
+            String code) {
+
+        Map<String, E> mapping = (Map<String, E>) CACHE.computeIfAbsent(
+                enumType,
+                clazz -> Arrays
+                        .stream(enumType.getEnumConstants())
+                        .collect(Collectors.toUnmodifiableMap(IdentifiableCode::getCode, Function.identity()))
+        );
+
+        return mapping.get(code);
     }
 }
--- src/main/java/com/msm/core/security/enums/PermissionAction.java
+++ src/main/java/com/msm/core/security/enums/PermissionAction.java
@@ -5,11 +5,40 @@ import lombok.Getter;
 
 @Getter
 public enum PermissionAction implements IdentifiableCode {
-    VIEW("V"),
     CREATE("C"),
+    VIEW("V"),
     UPDATE("U"),
     DELETE("D"),
-    APPROVE("A");
+
+    COPY("Y"),
+    REVISE("R"),
+
+    WON("W"),
+    LOST("L"),
+
+    CANCEL("N"),
+    REOPEN("O"),
+
+    PRINT("P"),
+    IMPORT("I"),
+    EXPORT("E"),
+
+    APPROVE("A"),
+    REJECT("J"),
+    SUBMIT("S"),
+
+    ALLOCATE("T"),
+    RETURN("B"),
+
+    REPLACE("H"),
+    EXCHANGE("X"),
+
+    LOOKUP("K"),
+
+    QUALIFY("Q"),
+    DISQUALIFY("DQ"),
+
+    SHARE("SH");
 
     private final String code;
 
@@ -17,13 +46,7 @@ public enum PermissionAction implements IdentifiableCode {
         this.code = code;
     }
 
-    public static IdentifiableCode fromCode(String code) {
-        for (IdentifiableCode action : values()) {
-            if (action.getCode().equals(code)) {
-                return action;
-            }
-        }
-
-        throw new IllegalArgumentException("Unknown action: " + code);
+    public static PermissionAction fromCode(String code) {
+        return IdentifiableCode.fromCode(PermissionAction.class, code);
     }
 }
